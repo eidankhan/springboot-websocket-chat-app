7b4b634a405fd478335add9170ed510275a33197
Eidan <eidankhan.student.muet@gmail.com>
2021-12-23 11:44:42 +0500

    Created Chat app with springboot and websocket

---
+++ .history/src/main/java/io/chatapp/WebSocketMessageBrokerConfigurer_20211223111243.java
@@ -0,0 +1,17 @@
+package io.chatapp;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
---
+++ .history/src/main/java/io/chatapp/WebsocketChatAppApplication_20211223113823.java
@@ -0,0 +1,14 @@
+package io.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketChatAppApplication.class, args);
+		System.out.println("App has been started successfully..........!!!!!!!!");
+	}
+
+}
